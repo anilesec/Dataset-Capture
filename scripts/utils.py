9af26c6b74feab54c2f2a82aa6e8a5268b87dc44
Anilkumar Erappanakoppal Swamy <aerappan@inrialpes.fr>
2022-06-25 06:18:13 +0200

    utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,3 +5,12 @@ def read_o3d_pcd(file_path):
         pcd = o3d.io.read_point_cloud(file_path)
 
         return pcd
+
+def write_o3d_pcd(file_path, pcd_o3d):
+        import open3d as o3d
+        import os
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        o3d.io.write_point_cloud(file_path, pcd_o3d, write_ascii=True)
+
+        return print(f"saved: {file_path}")
+
