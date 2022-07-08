2a36a03f37dd8b41ed552ee33d8c85ca029a2b76
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2022-07-08 14:56:29 +0200

    vid func added

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,3 +14,10 @@ def write_o3d_pcd(file_path, pcd_o3d):
 
         return print(f"saved: {file_path}")
 
+def imgs2vid_ffmpeg(imgs_dir, file_pth, ext='png',  frm_rate=10):
+    import os
+    print(f"ffmpeg creating video...")
+    cmd = f"ffmpeg -hide_banner -loglevel error -framerate {frm_rate} -pattern_type glob -i '{imgs_dir}/*.{ext}' -c:v " f"libx264 -vf fps=30 -pix_fmt yuv420p {file_pth} -y "
+    os.system(cmd)
+   # os.system(f"rm {imgs_dir}/*.jpg")s
+    return print(f"video saved here: {file_pth}")
