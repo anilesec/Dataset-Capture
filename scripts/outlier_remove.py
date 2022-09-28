d6ef4f00b39d597267bd2ffaa2c2f88ac919fcac
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2022-09-28 16:07:55 +0200

    latest changes

--- scripts/outlier_remove.py
+++ scripts/outlier_remove.py
@@ -14,9 +14,9 @@ def display_inlier_outlier(cloud, ind):
 	# inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
 	o3d.visualization.draw([inlier_cloud, outlier_cloud])
 
-def rm_outlier(inp_pcds_dir, seq_dir, vox_size, radius, num_nibrs, down_sample=True):
+def rm_outlier(inp_pcds_dir, seq_dir, vox_size, radius, num_nibrs, down_sample=True, start_ind=None, end_ind=None):
     pcds_pths = sorted(glob.glob(osp.join(inp_pcds_dir, '*.ply')))
-    for pcdp in tqdm(pcds_pths):
+    for pcdp in tqdm(pcds_pths[start_ind : end_ind]):
         pcd = o3d.io.read_point_cloud(pcdp)
 
         if down_sample:
@@ -26,9 +26,8 @@ def rm_outlier(inp_pcds_dir, seq_dir, vox_size, radius, num_nibrs, down_sample=T
             voxel_down_pcd = pcd
 
         print("Radius oulier removal...")
-        # cl, ind = voxel_down_pcd.remove_radius_outlier(nb_points=num_nibrs, radius=radius)
-        cl, ind = voxel_down_pcd.remove_statistical_outlier(nb_neighbors=200, std_ratio=1.5)
-
+        cl, ind = voxel_down_pcd.remove_radius_outlier(nb_points=num_nibrs, radius=radius)
+        # cl, ind = voxel_down_pcd.remove_statistical_outlier(nb_neighbors=400, std_ratio=0.01)
 
         # display_inlier_outlier(voxel_down_pcd, ind)
 
@@ -36,6 +35,7 @@ def rm_outlier(inp_pcds_dir, seq_dir, vox_size, radius, num_nibrs, down_sample=T
         fn_pcd = osp.join(seq_dir, 'fgnd_xyz_wc_wn_wo_outlr', osp.basename(pcdp))
         os.makedirs(osp.dirname(fn_pcd), exist_ok=True)
         voxel_down_pcd.estimate_normals()
+        # bb()
         # o3d.io.write_point_cloud(fname_pcd, voxel_down_pcd.select_by_index(ind))
         write_ply(fn_pcd, verts=np.array(voxel_down_pcd.select_by_index(ind).points), trias=None,
             color=(np.array(voxel_down_pcd.select_by_index(ind).colors)*255).astype(np.uint8),
@@ -67,12 +67,15 @@ if __name__ == "__main__":
                         help='voxel size for down sampling')
     parser.add_argument('--radius', type=float, default=0.01,
                         help='outlier search radius of sphere')
-    parser.add_argument('--num_nibrs', type=int, default=250,
+    parser.add_argument('--num_nibrs', type=int, default=200,
                         help='min no of points to be present in the sphere for inlier cond.')
-    
+    parser.add_argument('--start_ind', type=int, default=None,
+                        help='start index of a seq')
+    parser.add_argument('--end_ind', type=int, default=None,
+                        help='End index of a seq')
     args = parser.parse_args()
     print("args:", args)   
     
-    rm_outlier(args.inp_pcds_dir, args.seq_dir, args.vox_size, args.radius, args.num_nibrs, down_sample=True)
+    rm_outlier(args.inp_pcds_dir, args.seq_dir, args.vox_size, args.radius, args.num_nibrs, down_sample=True, start_ind=args.start_ind, end_ind=args.end_ind)
 
     print('Done!')
