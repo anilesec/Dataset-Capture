3b90420ab49abbb9cceace600cc8b44b589eaa32
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2022-09-28 16:08:30 +0200

    latest changes

--- scripts/run_icp_tgt_fltrd.py
+++ scripts/run_icp_tgt_fltrd.py
@@ -1,8 +1,11 @@
+
 if True:
     import open3d as o3d
-    from scripts.io3d import *
+    # from scripts.io3d import *
+    from io3d import *
     import numpy as np
     import os, sys
+    import time
     osp = os.path
 
     def remove_farther_tgtpts(src, tgt, dist_thresh=0.01):
@@ -34,6 +37,7 @@ if True:
         return print(curr_frm_init_pcd_write_pth)
 
     def filtr_tgt_pcd(tgt_pcd_pth, curr_frm_init_pcd_pth, tgt_fltrd_pcd_write_pth, dist_thresh):
+        # bb()
         tgt_pcd = o3d.io.read_point_cloud(tgt_pcd_pth)
         curr_frm_pcd = o3d.io.read_point_cloud(curr_frm_init_pcd_pth)
 
@@ -71,7 +75,8 @@ if True:
         return print(f'final pcd:{curr_frm_f_pcd_write_pth}, final trnsfm:{curr_frm_f_write_pth}')
 
 
-    seq = '20220804165147'
+    seq = '20220905155551'
+
     if seq == '20220705173214':
         for frm_idx in range(800, 900):
             print(f"\n frm_idx: {frm_idx}")
@@ -132,38 +137,47 @@ if True:
             curr_frm_f_write_pth = f'/scratch/github_repos/Fast-Robust-ICP/res/{seq}/frm{frm_idx}/f_trans.txt'
             curr_frm_f_pcd_write_pth = f'/scratch/github_repos/Fast-Robust-ICP/res/{seq}/frm{frm_idx}/f_pcd.ply'
             finalize(curr_frm_pcd_pth, curr_frm_o_pth, curr_frm_i_pth, curr_frm_f_write_pth, curr_frm_f_pcd_write_pth)
-    elif seq == '20220804165147':
-        for frm_idx in range(1, 915):
-            ICP_DIR = '/home/aswamy/my_drive/github_repos/Fast-Robust-ICP'
-            # if frm_idx < 676     :
+    elif seq == '20220905155551':
+        for frm_idx in range(1, 1250):
+            start = time.time()
+            ICP_DIR = '/scratch/1/user/aswamy/github_repos/Fast-Robust-ICP'
+            SAVE_DIR = '/scratch/1/user/aswamy/data/hand-obj'
+            # if frm_idx < 156     :
             #     continue
             # bb()
             print(f"\n frm_idx: {frm_idx}")
             # Step1 (create src initialzation)
             curr_frm_pcd_pth = f"/{ICP_DIR}/data/{seq}/data_for_reg/{frm_idx:010d}.ply"
-            prev_frm_f_pth = f'/{ICP_DIR}/res/{seq}/{(frm_idx - 1):010d}/f_trans.txt'
+            prev_frm_f_pth = f'/{SAVE_DIR}/{seq}/icp_res/{(frm_idx - 1):010d}/f_trans.txt'
             curr_frm_init_pcd_write_pth = f'{ICP_DIR}/data/{seq}/data_for_reg/init_{frm_idx:010d}.ply'
             initialize(curr_frm_pcd_pth, prev_frm_f_pth, curr_frm_init_pcd_write_pth)
             
             # Step2 (filter target pcd)
             # icp_dir = '/scratch/github_repos/Fast-Robust-ICP/'
-            tgt_pcd_pth = f"{osp.join(ICP_DIR, f'data/{seq}/data_for_reg/tgt_pcd.ply')}"
+            tgt_pcd_pth = f"{osp.join(SAVE_DIR, f'{seq}/gt_mesh/xyz/tgt_pcd.ply')}"
             curr_frm_init_pcd_pth = f"{osp.join(ICP_DIR, f'data/{seq}/data_for_reg/init_{frm_idx:010d}.ply')}"
             tgt_fltrd_pcd_write_pth = f"{osp.join(ICP_DIR, f'data/{seq}/data_for_reg/tgt_fltrd_{frm_idx:010d}.ply')}"
-            filtr_tgt_pcd(tgt_pcd_pth, curr_frm_init_pcd_pth, tgt_fltrd_pcd_write_pth, dist_thresh=0.005)
+            filtr_tgt_pcd(tgt_pcd_pth, curr_frm_init_pcd_pth, tgt_fltrd_pcd_write_pth, dist_thresh=0.01)
 
             # Step3 (run FRICP between src_init and tgt_fltrd)  
             # icp_dir = '/scratch/github_repos/Fast-Robust-ICP'
-            res_dir = osp.join(ICP_DIR ,f'res/{seq}/{frm_idx:010d}')
+            res_dir = osp.join(SAVE_DIR ,f'{seq}/icp_res/{frm_idx:010d}')
             os.makedirs(res_dir, exist_ok=True)
             FRICP(icp_dir=ICP_DIR, tgt_fltrd_pcd_pth=tgt_fltrd_pcd_write_pth, src_init_pcd_pth=curr_frm_init_pcd_write_pth, res_dir=res_dir)
-
+            # bb()
             # Step4 (save final registered mesh and transformation)
             curr_frm_pcd_pth = f"{ICP_DIR}/data/{seq}/data_for_reg/{frm_idx:010d}.ply"
-            curr_frm_o_pth = f'{ICP_DIR}/res/{seq}/{frm_idx:010d}/m3trans.txt'
-            curr_frm_i_pth = f'{ICP_DIR}/res/{seq}/{(frm_idx - 1):010d}/f_trans.txt'
-            curr_frm_f_write_pth = f'{ICP_DIR}/res/{seq}/{frm_idx:010d}/f_trans.txt'
-            curr_frm_f_pcd_write_pth = f'{ICP_DIR}/res/{seq}/{frm_idx:010d}/f_pcd.ply'
+            curr_frm_o_pth = f'{SAVE_DIR}/{seq}/icp_res/{frm_idx:010d}/m3trans.txt'
+            curr_frm_i_pth = f'{SAVE_DIR}/{seq}/icp_res/{(frm_idx - 1):010d}/f_trans.txt'
+            curr_frm_f_write_pth = f'{SAVE_DIR}/{seq}/icp_res/{frm_idx:010d}/f_trans.txt'
+            curr_frm_f_pcd_write_pth = f'{SAVE_DIR}/{seq}/icp_res/{frm_idx:010d}/f_pcd.ply'
             finalize(curr_frm_pcd_pth, curr_frm_o_pth, curr_frm_i_pth, curr_frm_f_write_pth, curr_frm_f_pcd_write_pth)
+            print(f'Frame {frm_idx} Time:: {(time.time() - start):0.4f}s')
             # bb()
+    # run metrics script
+    metrics_cmd = f"python /gfs-ssd/user/aswamy/github_repos/Dataset-Capture/scripts/metrics.py --sqn {seq}"
+    os.system(metrics_cmd)
+    # run img projection script
+    proj_gt2frms_cmd = f"python /gfs-ssd/user/aswamy/github_repos/Dataset-Capture/scripts/proj_gt2frms.py --sqn {seq}"
+    os.system(proj_gt2frms_cmd)
     print('Done!')
