1aa21d9a7366203205d7157d42e3396bc91d9bcb
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2023-12-03 18:02:28 +0100

    updated for consistent computation

--- evaluation/eval_colmap_rel_pose.py
+++ evaluation/eval_colmap_rel_pose.py
@@ -18,8 +18,7 @@ def create_o3d_pcd(verts, clr=np.array([[0., 0., 1.]])):
     pcd.points = o3d.utility.Vector3dVector(np.array(verts))
 
     clrs = np.repeat(clr, len(verts), axis=0)
-    # pcd.colors = o3d.utility.Vector3dVector(np.array(verts))
-
+    pcd.colors = o3d.utility.Vector3dVector(np.array(verts))
     return pcd
 
 def write_o3d_pcd(file_path, pcd_o3d):
@@ -87,18 +86,20 @@ if __name__ == "__main__":
         assert args.sqn in all_sqns, f"{args.sqn} is not present in listed sequences!!!"
         all_sqns = [args.sqn]
 
-    all_sqns_str = "20220805164755 20220829155218 20220902153221 20220824154342 20220830162330 20220809161015 20220905151029 20220905155551 20220905153946 20220824104203 20220913145135 20220812180133 20220907153810 20220905154829 20220905111237 20220902154737 20220913151554 20220909114359 20220912144751 20220909145039 20220909140016 20220902163904 20220811170459 20220705173214 20220912155637 20220824142508 20220905140306 20220824160141 20220805165947 20220902114024 20220830161218 20220902111535 20220902104048 20220909151546 20220824152850 20220912161700 20220909111450 20220824150228 20220913153520 20220824105341 20220811172657 20220912160620 20220909113237 20220823113402 20220902111409 20220809163444 20220819155412 20220824181949 20220909142411 20220912151849 20220902151726 20220811165540 20220811163525 20220907155615 20220909134639 20220909120614 20220912143756 20220905105332 20220902170443 20220905112733 20220913144436 20220823115809 20220902110304 20220902163950 20220912164407 20220819162529 20220823114538 20220905142354 20220812170512 20220809171854 20220829154032 20220912165455 20220913154643 20220811171507 20220909115705 20220824155144 20220830163143 20220909152911 20220824144438 20220902164854 20220905112623 20220907152036 20220905141444 20220812174356 20220912161552 20220909141430 20220824180652 20220909121541 20220819164041 20220912142017 20220912152000 20220809170847 20220824102636 20220902115034 20220812172414 20220811154947"
-    all_sqns = all_sqns_str.split(' ')
-
     clmp_pose_missing_sqns = []
     clmp_pose_valid_sqns = []
     all_seqs_mean_RRE = []
     all_seqs_mean_RTE = []
     for sqn in tqdm(all_sqns):
         print('sqn:', sqn)
-
         # get cam poses
         cps_pths_clmp = sorted(glob.glob(osp.join(COLMAP_BASE_DIR, sqn, 'cam_poses/*.txt')))
+
+        all_gt_poses_pths = sorted(glob.glob(osp.join(RES_DIR, sqn, 'icp_res/*/f_trans.txt')))
+        print(f"colmap yields {len(cps_pths_clmp)} / {len(all_gt_poses_pths)} poses")
+        print(f'detection rate: {len(cps_pths_clmp) / len(all_gt_poses_pths)*100:.2f}')
+
+        bb()
         # cps_pths_ann = sorted(glob.glob(osp.join(RES_DIR, sqn, 'icp_res/*/f_trans.txt')))
 
         # if len(cps_pths_clmp) != len(cps_pths_ann):
@@ -115,6 +116,21 @@ if __name__ == "__main__":
         clmp_pose_valid_sqns.append(sqn)
         cps_clmp = np.array([np.loadtxt(cp_pth_clmp) for cp_pth_clmp in tqdm(cps_pths_clmp)])
         cps_ann = np.array([np.linalg.inv(np.loadtxt(cp_pth_ann)) for cp_pth_ann in tqdm(cps_pths_ann)])
+
+        # save camera centers
+        if False:
+            import trimesh
+            base_savedir = f"/scratch/2/user/aswamy/projects/HandObjectRelPose/out/cam_poses_viz/showme/{sqn}/cam_centers"
+            os.makedirs(base_savedir, exist_ok=True)
+            pcd_ann = trimesh.points.PointCloud(np.linalg.inv(cps_ann)[:, :3, 3])
+            # pcd_ann.export(f'{base_savedir}/cam_centers_ann.ply');
+            pcd_clmp = trimesh.points.PointCloud(np.linalg.inv(cps_clmp)[:, :3, 3]) # -R.T @ t (=inv(pose))
+            # pcd_clmp.export(f'{base_savedir}/cam_centers_clmp.ply');
+            tran_algnd, trnsfm = compute_similarity_transform(pcd_clmp.vertices, pcd_ann.vertices, return_transfom=True)
+            pcd_clmp_algnd = trimesh.points.PointCloud(tran_algnd)
+            # pcd_clmp_algnd.export(f'{base_savedir}/cam_centers_clmp_algnd.ply');
+            abs_cam_centers_err = np.linalg.norm((pcd_ann.vertices - pcd_clmp_algnd.vertices), axis=1)
+            print(f'Camera Centers Error: {abs_cam_centers_err.mean():.3f} m')
 
         # plot translations as its
         cps_clmp_tran = cps_clmp[:, :3, 3]
@@ -160,6 +176,50 @@ if __name__ == "__main__":
         all_rel_tran_clmp = np.array([(cps_clmp_tran_algnd[i] - cps_clmp_tran_algnd[0]) for i in range(1, len(cps_clmp_tran_algnd))])
         all_rel_tran_ann = np.array([(cps_ann_tran[i] - cps_ann_tran[0]) for i in range(1, len(cps_ann_tran))])
 
+
+        # translation error: (uncomment below block to compute error wrt to all ref-frame)
+        abs_tran_err = np.linalg.norm((cps_ann_tran - cps_clmp_tran_algnd), axis=1)
+        print(f'Absolute Translation Error: {abs_tran_err.mean():.3f} m')
+        rel_tran_gt = cps_ann_tran[:, None, :] - cps_ann_tran
+        rel_tran_est = cps_clmp_tran_algnd[:, None, :] - cps_clmp_tran_algnd
+        rel_tran_err = np.linalg.norm((rel_tran_gt-rel_tran_est), axis=2)
+        print(f'Rel Tran. Err.: {rel_tran_err.mean():.3f} m')
+
+        import roma, torch
+        def compute_relrot_mat_geodist(gt_rots, est_rots):
+            # relrot_mat_estim = np.linalg.inv(est_rots[:, None, :3, :3]) @ est_rots[:, :3, :3]
+            # relrot_mat_gt = np.linalg.inv(gt_rots[:, None, :3, :3]) @ gt_rots[:, :3, :3]
+            relrot_mat_estim = est_rots[:, :3, :3] @ np.linalg.inv(est_rots[:, None, :3, :3])
+            relrot_mat_gt = gt_rots[:, :3, :3] @ np.linalg.inv(gt_rots[:, None, :3, :3])
+            relrot_diff = roma.rotmat_geodesic_distance_naive(torch.tensor(relrot_mat_gt), torch.tensor(relrot_mat_estim)) * 180 / np.pi
+            print(f"RR Mat. Geod. Dist. w.r.t ALL ref frms : {(relrot_diff).mean():.5f} deg")
+            return relrot_diff
+        def compute_relrot_amp_geodist(gt_rots, est_rots, ref_frm_ind=None):
+            # err wrt all ref frames
+            relrot_allrefs_gt = np.array(
+                roma.rotmat_geodesic_distance_naive(torch.tensor(gt_rots[:, None, :3, :3]), torch.tensor(gt_rots[:, :3, :3]))
+                ) * 180 / np.pi
+            relrot_allrefs_est = np.array(
+                roma.rotmat_geodesic_distance_naive(torch.tensor(est_rots[:, None, :3, :3]), torch.tensor(est_rots[:, :3, :3]))
+                ) * 180 / np.pi
+            rel_rot_diff = np.abs(relrot_allrefs_gt - relrot_allrefs_est)
+            print(f"RR Amp. Geod. Dist. w.r.t ALL ref frms : {rel_rot_diff.mean():.5f} deg")
+
+            # err with single ref frame
+            if ref_frm_ind is not None:
+                rel_rot_estim = np.array(
+                        [roma.rotmat_geodesic_distance_naive(torch.tensor(est_rots[idx, :3, :3]), torch.tensor(est_rots[ref_frm_ind, :3, :3]))
+                        for idx in range(len(est_rots))]
+                        ) * 180 / np.pi
+                rel_rot_gt = np.array(
+                        [roma.rotmat_geodesic_distance_naive(torch.tensor(gt_rots[idx, :3, :3]), torch.tensor(gt_rots[ref_frm_ind, :3, :3]))
+                        for idx in range(len(gt_rots))]
+                        ) * 180 / np.pi
+                rel_rot_diff = np.abs(rel_rot_estim - rel_rot_gt)
+                print(f"RR Geod. Dist. w.r.t ref_frm_ind-{ref_frm_ind} : {(np.abs(rel_rot_diff).mean()):.5f} deg")
+        relrot_err_mat = compute_relrot_mat_geodist(gt_rots=cps_ann[:, :3, :3], est_rots=cps_clmp[:, :3, :3]);
+        relrot_err_amp = compute_relrot_amp_geodist(gt_rots=cps_ann[:, :3, :3], est_rots=cps_clmp[:, :3, :3], ref_frm_ind=None);
+
         # ref relative rot
         all_rel_rot_clmp = np.array(
             [cps_clmp[idx, :3, :3] @ np.linalg.inv(cps_clmp[0, :3, :3])
@@ -169,7 +229,6 @@ if __name__ == "__main__":
             [cps_ann[idx, :3, :3] @ np.linalg.inv(cps_ann[0, :3, :3])
             for idx in range(1, len(cps_ann))]
             )
-        
         # ref-rel tran error(after alignment)
         all_rel_tran_err = np.linalg.norm((all_rel_tran_clmp - all_rel_tran_ann), axis=1)
 
@@ -178,13 +237,32 @@ if __name__ == "__main__":
         [(geodesic_distance_for_rotations(all_rel_rot_clmp[i, :3, :3], all_rel_rot_ann[i, :3, :3]) * (180 / np.pi))
         for i in range(1, len(all_rel_rot_clmp))]
         )
-
-        disp_rot_err(all_rel_rot_err)
-        disp_tran_err(all_rel_tran_err)
+        # disp_rot_err(all_rel_rot_err)
+        # disp_tran_err(all_rel_tran_err)
+        # bb()
+        # ref relative rot (this is not correct because it gives 
+        # higher error but I do not know why)
+        # all_rel_rot_clmp_inv = np.array(
+        #     [np.linalg.inv(cps_clmp[idx, :3, :3]) @ cps_clmp[0, :3, :3]
+        #     for idx in range(1, len(cps_clmp))]
+        #     )
+        # all_rel_rot_ann_inv = np.array(
+        #     [np.linalg.inv(cps_ann[idx, :3, :3]) @ cps_ann[0, :3, :3]
+        #     for idx in range(1, len(cps_ann))]
+        #     )
+        # # rel-rel rot error
+        # all_rel_rot_err_inv = np.array(
+        # [(geodesic_distance_for_rotations(all_rel_rot_clmp_inv[i, :3, :3], all_rel_rot_ann_inv[i, :3, :3]) * (180 / np.pi))
+        # for i in range(1, len(all_rel_rot_clmp_inv))]
+        # )
+        # disp_rot_err(all_rel_rot_err_inv)
 
         all_seqs_mean_RRE.append(all_rel_rot_err.mean())
         all_seqs_mean_RTE.append(all_rel_tran_err.mean())
+        bb()
 
+
+        
         print('Saving the rel pose error')
         save_dict = {
             'sqn' : sqn,
@@ -203,7 +281,6 @@ if __name__ == "__main__":
 
     all_seqs_mean_RRE = np.array(all_seqs_mean_RRE)
     all_seqs_mean_RTE = np.array(all_seqs_mean_RTE)
-
     print('Avg of mean-RRE of all seqs:', all_seqs_mean_RRE.mean())
     print('Avg of mean-RTE of all seqs:', all_seqs_mean_RTE.mean())
 
