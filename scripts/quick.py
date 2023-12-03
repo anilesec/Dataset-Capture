459dbc002b4b53b5bc935acdf3aea2370575a8d9
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2023-12-03 18:00:20 +0100

    script

--- scripts/quick.py
+++ scripts/quick.py
@@ -8,57 +8,60 @@ from tqdm import tqdm
 from ipdb import set_trace as bb
 import pandas as pd
 import numpy as np
+import pickle
+
+# tex_data = pd.read_csv('/Users/aswamy/My_drive/github_repos/Dataset-Capture/dope_hhor_recon_tex_notok.csv')
+# nonsmall_data = pd.read_csv('/Users/aswamy/My_drive/github_repos/Dataset-Capture/briac_recon_colmap_non-small_objs.csv')   
+# bb()
 
 
 
+# get small_obj_sqns
+
+# get large obj sqns
+
+# get good tex sqns
+
+# get not good tex sqn
 
 
-pths = ["/scratch/1/user/aswamy/data/briac_baseline/20220705173214/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220809161015/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220811154947/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220812172414/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220812180133/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220823115809/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220824142508/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220902170443/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220905153946/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220907155615/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220909114359/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220912142017/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220912143756/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220913145135/briac_recon_eval_sample200000_dth_0.0050.pkl",
-"/scratch/1/user/aswamy/data/briac_baseline/20220913151554/briac_recon_eval_sample200000_dth_0.0050.pkl"]
 
+# # load all object meshes
+# obj_pths = glob.glob('/scratch/1/user/aswamy/data/hand-obj/object_meshes/*/obj_mesh/*.obj')
+# import trimesh
+
+# meshes = dict()
+# for p in tqdm(obj_pths):
+#     meshes[osp.basename(p)] = trimesh.load(p)
+# keys = list(meshes.keys())
+# bb()
+
+# print('Done!')
+
+
+showme_tes_sqns = os.listdir('/scratch/2/user/aswamy/projects/hhor_evaluation/HHOR_v2/HHOR/NeuS/exp/showme_sra_tra/subsample_showme-sra-tra_max60_iter100k_precomp_radius_origin')
+base_dir = '/scratch/2/user/aswamy/projects/hhor_evaluation/HHOR_v2/HHOR/NeuS/exp/showme_sra_tra/subsample_showme-sra-tra_max60_iter100k_precomp_radius_origin'
 all_seqs_dict = []
-for pth in pths:
-    # pkl_pth_5mm = osp.join(VH_RECON_RES_DIR, sqn, 'vh_recon_eval_sample200000_dth_0.0050.pkl')
-    # pkl_pth = pathlib.Path(osp.join(BRIAC_RECON_RES_DIR, sqn, args.pkl_name))
-    # import pathlib
-    # if not pkl_pth.exists():
-    #     print('missing pkl:', pkl_pth)
-    #     continue
-    sqn = pth.split('/')[-2]
-    data = load_pkl(str(pth))
-    # data.update({'sqn' : sqn})
-    
-    new_dict = {
-        'sqn' : sqn,
-        'accu' : f"{data['accuracy_rec']:.4f}",
-        'comp' : f"{data['completion_rec']:.4f}",
-        'precision_ratio' : f"{data['precision_ratio_rec']*100:.4f}",
-        'comp_ratio' : f"{data['completion_ratio_rec']*100:.4f}",
-        'fscore' : f"{data['fscore']:.4f}",
-        'normal_acc' : f"{data['normal_acc']:.4f}",
-        'normal_comp' : f"{data['normal_comp']:.4f}",
-        'normal_avg' : f"{data['normal_avg']:.4f}",
-    }
-
-    # bb()
-    df = pd.DataFrame.from_dict([new_dict])
-    all_seqs_dict.append(df)
+for sqn in showme_tes_sqns:
+    pklpth = os.path.join(base_dir, sqn, 'eval_meshes_256/sra_tra_hhor_recon_eval_sample20000_dth_0.005.pkl')
+    with open (pklpth, 'rb') as f:
+        data = pickle.load(f)
+        new_dict = {
+            'sqn' : sqn,
+            'accu' : f"{data['accuracy_rec']*100:.4f}",
+            'comp' : f"{data['completion_rec']*100:.4f}",
+            'precision_ratio' : f"{data['precision_ratio_rec']*100:.4f}",
+            'comp_ratio' : f"{data['completion_ratio_rec']*100:.4f}",
+            'fscore' : f"{data['fscore']:.4f}",
+            # 'normal_acc' : f"{data['normal_acc']:.4f}",
+            # 'normal_comp' : f"{data['normal_comp']:.4f}",
+            # 'normal_avg' : f"{data['normal_avg']:.4f}",
+        }
+        df = pd.DataFrame.from_dict([new_dict])
+        all_seqs_dict.append(df)
 
 df_comb = pd.concat(all_seqs_dict)
-# df_comb_save_pth = osp.join(RES_DIR, args.pkl_name.replace('pkl', 'csv'))
-df_comb.to_csv('./missing_briac_evals.csv')
-
+df_comb_save_pth = osp.join(f'{base_dir}.csv')
+df_comb.to_csv(df_comb_save_pth)
+print(f"Saved here: {df_comb_save_pth}")
 print('Done!!')
