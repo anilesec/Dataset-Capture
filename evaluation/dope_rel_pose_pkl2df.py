96902e5660a5b384fb48766600104676f199d4da
Anilkumar Swamy <anilkumar.swamy-intern@naverlabs.com>
2023-12-03 18:02:41 +0100

    updated for consistent computation

--- evaluation/dope_rel_pose_pkl2df.py
+++ evaluation/dope_rel_pose_pkl2df.py
@@ -35,37 +35,50 @@ if __name__ == "__main__":
 
         pkl_pth = osp.join(RES_DIR, sqn, 'eval_rel_pose_dope.pkl')
         data = load_pkl(pkl_pth)
+
+        ths = [[4.0, 0.02], [10.0, 0.05], [20.0, 0.10]]
+        bins = []
+        for th in ths:
+            # bb()
+            bin_vals = np.logical_and(data['RRE'] <= th[0], data['RTE'][:-1] <= th[1])
+            bins.append(float(f"{np.average(bin_vals)*100:.2f}"))
         new_dict = {
             'sqn' : data['sqn'],
-            'repr_mean' : f"{data['REPE_mean']:.4f}",
-            'rre_mean' : f"{np.mean(data['RRE']):.4f}",
-            'rre_med' : f"{np.median(data['RRE']):.4f}",
-            'rre_max' : f"{np.max(data['RRE']):.4f}",
-            'rre_min' : f"{np.min(data['RRE']):.4f}",
-            'rre_std' : f"{np.std(data['RRE']):.4f}",
-            'rre_25th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[0]:.4f}",
-            'rre_50th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[1]:.4f}",
-            'rre_75th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[2]:.4f}",
-            'rre_95th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[3]:.4f}",
-            'rte_mean' : f"{np.mean(data['RTE']):.4f}",
-            'rte_med' : f"{np.median(data['RTE']):.4f}",
-            'rte_max' : f"{np.max(data['RTE']):.4f}",
-            'rte_min' : f"{np.min(data['RTE']):.4f}",
-            'rte_std' : f"{np.std(data['RTE']):.4f}",
-            'rte_25th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[0]:.4f}",
-            'rte_50th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[1]:.4f}",
-            'rte_75th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[2]:.4f}",
-            'rte_95th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[3]:.4f}",
-            'miss_frms_no' : f"{data['missing_det_info'][1]:.4f}",
-            'miss_frms_ratio' : f"{data['missing_det_info'][2]:.4f}"    
+            f"rre_{ths[0][0]}_rte{ths[0][1]}" : bins[0], 
+            f"rre_{ths[1][0]}_rte{ths[1][1]}" : bins[1],
+            f"rre_{ths[2][0]}_rte{ths[2][1]}" : bins[2]  
         }
+        # new_dict = {
+        #     'sqn' : data['sqn'],
+        #     'repr_mean' : f"{data['REPE_mean']:.4f}",
+        #     'rre_mean' : f"{np.mean(data['RRE']):.4f}",
+        #     'rre_med' : f"{np.median(data['RRE']):.4f}",
+        #     'rre_max' : f"{np.max(data['RRE']):.4f}",
+        #     'rre_min' : f"{np.min(data['RRE']):.4f}",
+        #     'rre_std' : f"{np.std(data['RRE']):.4f}",
+        #     'rre_25th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[0]:.4f}",
+        #     'rre_50th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[1]:.4f}",
+        #     'rre_75th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[2]:.4f}",
+        #     'rre_95th' : f"{np.percentile(data['RRE'], [25, 50, 75, 90])[3]:.4f}",
+        #     'rte_mean' : f"{np.mean(data['RTE']):.4f}",
+        #     'rte_med' : f"{np.median(data['RTE']):.4f}",
+        #     'rte_max' : f"{np.max(data['RTE']):.4f}",
+        #     'rte_min' : f"{np.min(data['RTE']):.4f}",
+        #     'rte_std' : f"{np.std(data['RTE']):.4f}",
+        #     'rte_25th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[0]:.4f}",
+        #     'rte_50th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[1]:.4f}",
+        #     'rte_75th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[2]:.4f}",
+        #     'rte_95th' : f"{np.percentile(data['RTE'], [25, 50, 75, 90])[3]:.4f}",
+        #     'miss_frms_no' : f"{data['missing_det_info'][1]:.4f}",
+        #     'miss_frms_ratio' : f"{data['missing_det_info'][2]:.4f}"    
+        # }
 
         df = pd.DataFrame.from_dict([new_dict])
         all_seqs_dict.append(df)
 
     df_comb = pd.concat(all_seqs_dict)
 
-    df_comb_save_pth = osp.join(RES_DIR, 'all_seqs_dope_rel_pose_eval.csv')
+    df_comb_save_pth = osp.join(RES_DIR, 'all_seqs_dope_rel_pose_eval_perframe.csv')
     df_comb.to_csv(df_comb_save_pth)
     print(f"Saved here: {df_comb_save_pth}")
     print('Done!!')
